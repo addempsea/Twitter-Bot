b82c90e87178adb1f8836502426a81c0d9cd3f3b
addempsea <babatundeademola4@gmail.com>
2019-12-31 21:12:00 +0100

    BOP

---
+++ app.js
@@ -0,0 +1,54 @@
+
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var request = require('request');
+
+var Twit = require("twit");
+
+var config = require('./config')
+
+
+var T = new Twit(config)
+
+var app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var express = require('express');
+
+function demo () {
+    request('https://type.fit/api/quotes', { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        
+        var min =1; 
+        var max =1643;  
+        var r = Math.floor(Math.random() * (+max - +min)) + +min;
+        
+
+        function tweetIt() {
+            var tweet = {
+                status: body[r].text + " - " + body[r].author
+            }
+
+            T.post('statuses/update', tweet, tweeted)
+
+            function tweeted(err, data, response) {
+                if (err) {
+                    console.log('something went wrong');
+                } else {
+                    console.log('Tweet sent');
+                }
+            }
+        }
+
+        return tweetIt();
+    });
+}
+
+demo()
+setInterval(demo, 1000*60*60);
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'rziUUkTEm6BmtmiH5zJgeBh64',
+    consumer_secret:      'H90SlAosU0gw556oZD7D3OTbzll8KpkI0OLo1TlxKc5BGjvpju',
+    access_token:         '1196874000837816320-TL5yF3u1wBQDrhvjppwnROdCf12tZy',
+    access_token_secret:  'WZnqUdLY9J4e2bY75VWt3mULopKaDZCt7ACadGdUvFRk6',
+}
