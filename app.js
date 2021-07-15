d0bc824951ed07b9f0d0f3f2b1dcc3414bbe4ca2
addempsea <babatundeademola4@gmail.com>
2021-07-15 17:13:29 +0100

    fix: 🐛 handle undefined posts

--- app.js
+++ app.js
@@ -15,30 +15,33 @@ function tweeted(error, data, response) {
     console.log(error.twitterReply);
     console.log("something went wrong");
   } else {
-    console.log('Tweet sent');
+    console.log("Tweet sent");
   }
 }
 
 function postQuotes() {
-  request("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json", { json: true }, function (
-    err,
-    res,
-    body
-  ) {
-    if (err) {
-      return console.log(err);
-    }
+  request(
+    "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json",
+    { json: true },
+    function (err, res, body) {
+      if (err) {
+        return console.log(err);
+      }
 
-    function tweetIt() {
-      const tweet = {
-        status: body.quoteText + " - " + `${body.quoteAuthor || 'Unknown'}` ,
-      };
-      T.post("statuses/update", tweet, tweeted);
+      function tweetIt() {
+        const tweet = {
+          status: body.quoteText + " - " + `${body.quoteAuthor || "Unknown"}`,
+        };
+        if (body.quoteText) {
+          T.post("statuses/update", tweet, tweeted);
+        } else {
+          console.log("no quote found");
+        }
+      }
+      return tweetIt();
     }
-    return tweetIt();
-  });
+  );
 }
 
 postQuotes();
 setInterval(postQuotes, 1000 * 60 * 60);
-
